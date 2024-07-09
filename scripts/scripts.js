ee634f9f1ab30ba9e1197de67d1f0421b6fa58a8
Sergei Serbulatov <ssergey.tlt@gmail.com>
2024-07-10 02:00:44 +0400

    I added the go to top button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -41,26 +41,30 @@ updateScrollPaddingTop();
 
 /* MOBILE MENU */
 const hamburger = document.getElementById("hamburger");
-const closeButton = document.getElementById("closeButton");
+const fadeMobile = document.getElementById("fadeMobile");
 const nav = document.getElementById("nav");
+const closeButton = document.getElementById("closeButton");
 const navItems = document.querySelectorAll(".nav-item");
 const body = document.body;
 
 hamburger.addEventListener("click", () => {
   const isNavOpen = nav.classList.toggle("nav-mobile");
   body.classList.toggle("lock-scroll", isNavOpen);
+  fadeMobile.classList.toggle("show-fade-mobile");
 });
 
 navItems.forEach((item) => {
   item.addEventListener("click", () => {
     nav.classList.remove("nav-mobile");
     body.classList.remove("lock-scroll");
+    fadeMobile.classList.remove("show-fade-mobile");
   });
 });
 
 closeButton.addEventListener("click", () => {
   nav.classList.remove("nav-mobile");
   body.classList.remove("lock-scroll");
+  fadeMobile.classList.remove("show-fade-mobile");
 });
 
 /* CHART.JS */
@@ -76,7 +80,7 @@ new Chart("pricingChart", {
       {
         backgroundColor: barColors,
         data: yValues,
-        barThickness: 50,
+        barThickness: 60,
       },
     ],
   },
@@ -152,4 +156,29 @@ if (contactForm) {
     fade.style.display = "none";
     notificationWindow.style.display = "none";
   });
+}
+
+// ------------ GO TO TOP BUTTON ------------- //
+document.addEventListener("DOMContentLoaded", () => {
+  const upButton = document.getElementById("goTop");
+  if (upButton) {
+    window.addEventListener("scroll", scrollFunction);
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        upButton.style.display = "block";
+      } else {
+        upButton.style.display = "none";
+      }
+    }
+  }
+});
+
+// When the user clicks on the button, scroll to the top of the document
+function goTopFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
